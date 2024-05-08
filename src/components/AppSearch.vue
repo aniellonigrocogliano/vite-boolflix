73011aa1da1de700e5954545abb5d75ece6ae9e4
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-05-08 21:18:00 +0200

    add: search input

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,32 @@
+<script>
+import { store } from "../store";
+export default {
+  data() {
+    return {
+      store,
+      itemSearch:"",
+    };
+  },
+  methods: {
+    getSearch() {
+      this.store.itemSearch=this.itemSearch;
+    },
+},
+};
+
+</script>
+
+<template>
+    <div class="container"> 
+        
+<div class="input-group mb-3">
+  <input v-model="itemSearch" type="text" class="form-control" placeholder="" aria-label="Recipient's username" aria-describedby="button-addon2">
+  <button @click="getSearch()" class="btn btn-outline-secondary" type="button" id="button-addon2">Cerca</button>
+</div>
+
+    </div>
+
+</template>
+<style scoped>
+
+</style>
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    keyId: "1ef69f2c2048fec983935e3fc944c5df",
+    itemSearch: "",
+    movieArray: [],
+    tvArray: [],
+});
