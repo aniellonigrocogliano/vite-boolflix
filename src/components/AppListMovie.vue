20e098caa48452d2bdea9479b1c4aa2629fa89f7
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-05-09 00:02:07 +0200

    add: bandiere per la lingua

--- src/components/AppListMovie.vue
+++ src/components/AppListMovie.vue
@@ -1,6 +1,5 @@
 <script>
 import { store } from "../store";
-
 export default {
   components: {
   },
@@ -9,18 +8,23 @@ export default {
       store,
     };
   },
-  
+  methods: {
+    getImagePath(imageName) {
+      return new URL(`../assets/img/flag/${imageName}.png`, import.meta.url).href;
+    },
+    
+  },
 };
 </script>
 
 <template>
   <div class="container">
-    
     <div class="row row-cols-4">
       <div class="col" v-for="movie in store.movieArray">
        <p>Titolo: {{movie.title}} </p>
        <p>Titolo originale: {{movie.original_title}} </p>
-       <p>Lingua: {{movie.original_language}} </p>
+       
+       <p>Lingua: <img :src="getImagePath(movie.original_language)" :alt="movie.original_language" /> </p>
        <p>Voto: {{movie.vote_average}} </p>
       </div>
     </div>
